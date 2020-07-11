71549cc88384dc674d15dcae03973294326e5134
eslam <emohamef733@gmail.com>
2020-07-11 08:12:59 +0200

    sa

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${port}!`));
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 }
 bootstrap();
